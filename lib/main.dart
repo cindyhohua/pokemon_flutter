bd110dd4ede18b39ac0cd75e2e229e0831336868
cindyhohua <cindyhohua@penpeer.co>
2024-10-13 20:58:15 +0800

    feat: add tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'PokemonDetailPage/pokemonDetail.dart';
 import 'PokemonModel/pokemonModel.dart';
 import 'pokemonCell.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'pokemonQuizPage.dart';
 
 void main() {
-    runApp(
-      ProviderScope( 
-        child: MyApp(),
-      ),
+  runApp(
+    ProviderScope(  
+      child: const MyApp(),
+    ),
   );
 }
 
@@ -23,7 +24,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const PokemonListPage(),
+      home: const MainPage(),  
+    );
+  }
+}
+
+class MainPage extends ConsumerStatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends ConsumerState<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const PokemonListPage(),
+    const PokemonQuizPage(),
+  ];
+
+  final List<BottomNavigationBarItem> _navItems = const [
+    BottomNavigationBarItem(icon: Icon(Icons.view_array_rounded), label: 'Pokedex'),
+    BottomNavigationBarItem(icon: Icon(Icons.create), label: 'Pokequiz'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: const Color.fromARGB(255, 255, 100, 100),
+        unselectedItemColor: const Color.fromARGB(255, 167, 150, 150),
+        backgroundColor: const Color.fromARGB(255, 52, 52, 52),
+        currentIndex: _currentIndex,
+        items: _navItems,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
